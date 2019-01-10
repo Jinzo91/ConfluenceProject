7aa6cd6363d14dea07d40197e509bbddcb1b9b3c
Jingzhou Wang <jingzhou.wang@tum.de>
2019-01-10 22:02:41 +0100

    added function to save all json to a file

--- ConfluenceConnector.py
+++ ConfluenceConnector.py
@@ -17,8 +17,17 @@ print("Content pages:")
 #print(getJson) #whole values
 # print(getJson['id']) #if only one key for each value exists
 
-# for i in getJson:
-#     print(i)
-
+content_list = []
+#gets every ID under a speciefied space
 for i in getJson['results']:
     print('ID: ' + i['id'])
+    id_content = api.get_content_by_id(i['id'], expand = 'body.storage')
+    #id_content = json.dumps(id_content, indent=2)
+    print(id_content)
+    content_list.append(json.dumps(id_content, indent=2))
+
+
+with open('data.json', 'w') as f:
+    json.dump(content_list, f, indent=2)
+
+
