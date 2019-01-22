2964b897c1422a0451c765a31690a63e0d281c22
Jingzhou Wang <jingzhou.wang@tum.de>
2019-01-22 19:17:25 +0100

    added more functions to the Rest API and improved the DB connection

--- Backend/ConfluenceConnector.py
+++ Backend/ConfluenceConnector.py
@@ -1,36 +1,25 @@
 from PythonConfluenceAPI import ConfluenceAPI
 import json
-from pymongo import MongoClient
-import requests
 
-#connection to mongodb
-client = MongoClient('localhost', 27017)
+def get_content():
+    api = ConfluenceAPI('se.bastian.esch@gmail.com', 'qUj9+UMj7Q', 'https://ibsolution.atlassian.net/wiki')
+    new_pages = api.get_content(expand=('body.storage,history,metadata.labels'))#grabs all content if no parameters specified
+    #id_content = api.get_content_by_id('117873296', expand = 'body.storage')
+    #id_child_content = api.get_content_children('2523167')
+    content_data = json.dumps(new_pages, indent=2)#careful: do not json.dump twice
+    getJson = json.loads(content_data)
+    #print(content_data)
+    print("Loading Confluence data...")
 
-api = ConfluenceAPI('se.bastian.esch@gmail.com', 'qUj9+UMj7Q', 'https://ibsolution.atlassian.net/wiki')
-new_pages = api.get_content(space_key='SAPTECH')
-#id_content = api.get_content_by_id('117873296', expand = 'body.storage')
-#id_child_content = api.get_content_children('2523167')
+    #Testing code
+    tags = []
+    labels = []
+    for i in getJson['results']:
+        for j in i['metadata']['labels']['results']:
+            tags.append(j['label'])
+        labels.append(tags)
+        tags = []
+    #print(labels)
+    return getJson
 
-content_data = json.dumps(new_pages, indent=2)
-getJson = json.loads(content_data)
-print("Content pages:")
-
-#print(content_data)
-
-#print(getJson) #whole values
-# print(getJson['id']) #if only one key for each value exists
-
-content_list = []
-#gets every ID under a speciefied space
-for i in getJson['results']:
-    print('ID: ' + i['id'])
-    id_content = api.get_content_by_id(i['id'], expand = 'body.storage')
-    id_content = json.dumps(id_content, indent=2)
-    print(id_content)
-    #content_list.append(json.dumps(id_content, indent = 4))
-
-#print(content_list)
-# with open('data.json', 'w') as f:
-#     json.dump(content_list, f, indent = 4)
-
-
+#get_content()
--- Backend/RestApi.py
+++ Backend/RestApi.py
@@ -2,9 +2,11 @@ from flask import Flask
 from flask import jsonify
 from flask import request
 from flask_pymongo import PyMongo
+from Backend.ConfluenceConnector import get_content
+
 
 app = Flask(__name__)
-
+#Check whether you have a database named projectdb, if not then create one.
 app.config['MONGO_DBNAME'] = 'projectdb'
 app.config['MONGO_URI'] = 'mongodb://localhost:27017/projectdb'
 
@@ -14,34 +16,44 @@ mongo = PyMongo(app)
 #lists all users and returns their names
 @app.route('/users', methods=['GET'])
 def get_all_users():
-  users = mongo.db.users
-  output = []
-  for u in users.find():
-    output.append({'name' : u['name']})
-  return jsonify({'result' : output})
+    users = mongo.db.users
+    output = []
+    for u in users.find():
+        output.append({'name' : u['name']})
+    return jsonify({'result' : output})
 
+
+@app.route('/users', methods=['POST'])
+def add_user():
+    users = mongo.db.users
+    name = request.json['name']
+    password = request.json['password']
+    new_user = users.insert({'name': name, 'password': password})
+    u = users.find_one({'_id': new_user})
+    output = {'name' : u['name']}
+    return jsonify({'result' : output}) #returns a json object and HTTP status code (eg. 200: success, 400: failure etc.)
 
 #------Confluence data methods-----#
 #lists all users and returns their names
 @app.route('/confluencedata', methods=['GET'])
-def get_all_confluencedata():
-  confluencedata = mongo.db.confluencedata
-  output = []
-  for u in confluencedata.find():
-    output.append({'documentId' : u['documentId'], 'title' : u['title'], 'date' : u['date'], 'body' : u['body'], 'tags' : u['tags']})
-  return jsonify({'result' : output})
+def get_all_confluenceData():
+    confluencedata = mongo.db.confluencedata
+    output = []
+    for u in confluencedata.find():
+        output.append({'documentId' : u['documentId'], 'title' : u['title'], 'date' : u['date'], 'body' : u['body'], 'tags' : u['tags']})
+    return jsonify({'result' : output}) #every return has HTTP status code
 
 
 #@ID: searches database and returns only matching data
 @app.route('/confluencedata/<Id>', methods=['GET'])
 def get_document_by_Id(Id):
-  confluencedata = mongo.db.confluencedata
-  s = confluencedata.find_one({'documentId' : Id})
-  if s:
-    output = {'documentId' : s['documentId'], 'title' : s['title'], 'date' : s['date'], 'body' : s['body'], 'tags' : s['tags']}
-  else:
-    output = "No document with ID: " + Id + " found."
-  return jsonify({'result' : output})
+    confluencedata = mongo.db.confluencedata
+    s = confluencedata.find_one({'documentId' : Id})
+    if s:
+        output = {'documentId': s['documentId'], 'title': s['title'], 'date': s['date'], 'body': s['body'], 'tags': s['tags']}
+    else:
+        output = "No document with ID: " + Id + " found."
+    return jsonify({'result' : output})
 
 #if-else clause searches and returns only matching data
 # @app.route('/confluencedata', methods=['GET'])
@@ -54,15 +66,30 @@ def get_document_by_Id(Id):
 #   #   output = "No such name"
 #   return jsonify({'result' : output})
 
-@app.route('/star', methods=['POST'])
-def add_star():
-  star = mongo.db.stars
-  name = request.json['name']
-  distance = request.json['distance']
-  star_id = star.insert({'name': name, 'distance': distance})
-  new_star = star.find_one({'_id': star_id })
-  output = {'name' : new_star['name'], 'distance' : new_star['distance']}
-  return jsonify({'result' : output})
+
+#downloads data from confluence API and saves it in the database
+@app.route('/confluencedata/download', methods=['GET'])
+def download_confluenceData():
+    confluenceData = mongo.db.confluencedata
+    documents = get_content()
+
+    #Mapping of json values from Confluence documents. Extracted json structure has to be the same as database structure.
+    for doc in documents['results']:
+        documentId = doc['id']
+        title = doc['title']
+        date = doc['history']['createdDate'][0:10]
+        body = doc['body']['storage']['value']
+        tags = []
+        labels =[]
+        for i in doc['metadata']['labels']['results']:
+            tags.append(i['label'])
+            labels.append(tags)
+            tags = []
+
+        confluenceData.update({'documentId': documentId},
+            {'documentId': documentId, 'title': title, 'date': date, 'body': body, 'tags': labels}, upsert=True)  # database structure
+
+    return ('Download successfull.')
 
 if __name__ == '__main__':
     app.run(debug=True)
